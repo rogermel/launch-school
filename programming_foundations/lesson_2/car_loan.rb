33abd9a0b837711f89f893099969430586740067
Rogermel <melg.roger@gmail.com>
2016-11-05 23:05:30 +0100

    Rock, Scissors Paper games

--- programming_foundations/lesson_2/car_loan.rb
+++ programming_foundations/lesson_2/car_loan.rb
@@ -22,27 +22,27 @@ loop do
       break
     end
   end
-  
+
   prompt 'What is the interest rate?'
   prompt '(Example: 5 for 5% or 2.5 for 2.5%)'
-  
+
   interest_rate = ''
   loop do
     interest_rate = gets.chomp
-    
+
     if invalid_value(interest_rate)
       prompt 'Must enter positive number.'
     else
       break
     end
   end
-  
+
   prompt "What is the loan duration (in years)?"
   years = ''
-  
+
   loop do
     years = gets.chomp
-    
+
     if invalid_value(years)
       prompt'Must enter positive number.'
     else
@@ -53,15 +53,16 @@ loop do
   annual_interest_rate = interest_rate.to_f / 100
   monthly_interest_rate = annual_interest_rate / 12
   months = years.to_i * 12
-  
+
   monthly_payment = amount.to_f *
-  (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-months.to_i))
-  
+                    (monthly_interest_rate /
+                    (1 - (1 + monthly_interest_rate)**-months.to_i))
+
   prompt "Your monthly payment is: $#{format('%02.2f', monthly_payment)}"
-  
+
   prompt "Another calculation?"
   answer = gets.chomp
-  
+
   break unless answer.downcase.start_with?('y')
 end
 
